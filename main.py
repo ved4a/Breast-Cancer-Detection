29ee120bbffe84c27e4c4a8b3afe64561748013f
Vedaa Anand <work.vedaa@gmail.com>
2024-12-19 15:47:32 +0530

    feat: implement custom logistic reg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ucimlrepo import fetch_ucirepo
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from logistic_reg import LogisticRegression
 
 import pandas as pd
 import numpy as np
@@ -24,3 +25,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
+
+# baseline model: logistic regression
+log_reg = LogisticRegression(learning_rate=0.01, iterations=1000)
+log_reg.fit(X_train, y_train)
+
+y_pred = log_reg.prediction(X_test)
