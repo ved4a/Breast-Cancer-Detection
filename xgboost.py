8331800449e55b8e32c8057ecd45bd4eb30c5785
Vedaa Anand <work.vedaa@gmail.com>
2024-12-29 13:18:27 +0530

    feat: create decision tree class

--- xgboost.py
+++ xgboost.py
@@ -10,3 +10,10 @@ class TreeNode:
 
     def is_leaf(self):
         return self.value is not None
+    
+class DecisionTree:
+    def __init__(self, max_depth=3, min_samples_split=2, reg_lambda=1.0):
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.reg_lambda = reg_lambda
+        self.root = None
