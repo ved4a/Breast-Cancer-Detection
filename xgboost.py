72621396de2c3d1d1d9743333b996026de074def
Vedaa Anand <work.vedaa@gmail.com>
2024-12-29 13:16:57 +0530

    feat: create tree node

---
+++ xgboost.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class TreeNode:
+    def __init__(self, feature=None, threshold=None, left=None, right=None, value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+
+    def is_leaf(self):
+        return self.value is not None
